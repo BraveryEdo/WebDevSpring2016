c04ed99b4a55bca6eed8a7690ccb82c5e6c5cdd3
BraveryEdo <Braveryedo@gmail.com>
2016-03-05 18:48:42 -0500

    assignment 2

--- public/assignment/views/main/main.controller.js
+++ public/assignment/views/main/main.controller.js
@@ -1,13 +1,17 @@
 /**
  * Created by EDO on 3/2/2016.
  */
+
+
 "use strict";
 (function(){
     angular
         .module("FormMakerApp", [])
         .controller("MainController", ['$scope', '$location', MainController]);
-
     function MainController($scope, $location){
         $scope.location = $location;
+        $scope.message = "MAIN";
     }
 })();
+
+
--- public/assignment/views/sidebar/sidebar.controller.js
+++ public/assignment/views/sidebar/sidebar.controller.js
@@ -2,12 +2,10 @@
  * Created by EDO on 3/2/2016.
  */
 "use strict";
-(function(){
     angular
         .module("FormMakerApp")
-        .controller('SidebarController', ['$rootScope', '$scope', '$location', SidebarController]);
+        .controller('SidebarController', ['$scope', '$location', SidebarController]);
 
-    function SidebarController($rootScope, $scope, $location) {
-        $scope.location = $location;
+    function SidebarController($scope, $location) {
+            $scope.location = $location;
     }
-})();
