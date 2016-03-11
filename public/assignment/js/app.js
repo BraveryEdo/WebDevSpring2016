6d853b843601b75b3808ac4e6fd1a3d7bc01b745
BraveryEdo <Braveryedo@gmail.com>
2016-03-11 16:12:53 -0500

    working instance of processing's javaScript library (p5.js) being run through an angularjs wrapper that I found on the processing community forums

--- public/assignment/js/app.js
+++ public/assignment/js/app.js
@@ -1,2 +1,2 @@
 "use strict";
-angular.module('FormMakerApp',['ngRoute']);
+angular.module('FormMakerApp',['ngRoute', 'angular-p5']);
--- public/project/views/home/project.controller.js
+++ public/project/views/home/project.controller.js
@@ -5,11 +5,33 @@
 (function(){
     angular
         .module("FormMakerApp")
+        .factory('viz', ['p5', function(p5) {
+            return function(p) {
+                //static variables
+
+                var r = p.random(0, 255);
+                var g = p.random(0, 255);
+
+                p.setup = function() {
+                    p.createCanvas(480, 270);
+                    p.noStroke();
+                };
+
+                p.draw = function() {
+                    var colorAngle = p.radians(p.frameCount);
+                    var colorVector = p5.Vector.fromAngle(colorAngle).setMag(255);
+
+                    p.background(r, g, colorVector.x);
+                    p.fill(r, g, colorVector.y);
+                    p.rect(0, 0, p.width / 2, p.height);
+                };
+            };
+        }])
         .controller("ProjectController", ['$scope', '$location', ProjectController]);
 
     function ProjectController($scope, $location){
-
         console.log("project controller finished loading");
     }
-})();
 
+
+})();
